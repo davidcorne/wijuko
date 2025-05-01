2f6fab5b66c1248498efcbbb22e437d71e346a02
David Corne <davidcorne@gmail.com>
2025-05-01 22:02:18 +0000

    Generate SVGs

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const wijuko = require('./wijuko')
 
 const main = function () {
   const puzzleRand = wijuko.generate(Math.random)
-  const puzzleFixed = wijuko.generateStandard1()
-  // Print the puzzle solved
-  let printGridNumber = true
-  puzzleRand.prettyPrint(console.log, printGridNumber)
-  // Print 20 blank lines
-  for (let i = 0; i < 20; i++) {
-    console.log('')
-  }
-  // Print the puzzle with the grid numbers hidden
-  printGridNumber = false
-  puzzleRand.prettyPrint(console.log, printGridNumber)
+  // // Print the puzzle solved
+  // let printGridNumber = true
+  // puzzleRand.prettyPrint(console.log, printGridNumber)
+  // // Print 20 blank lines
+  // for (let i = 0; i < 20; i++) {
+  //   console.log('')
+  // }
+  // // Print the puzzle with the grid numbers hidden
+  // printGridNumber = false
+  // puzzleRand.prettyPrint(console.log, printGridNumber)
+  const puzzleAsSVG = wijuko.generateSVG(puzzleRand)
+  console.log(puzzleAsSVG)
 }
 
 main()
