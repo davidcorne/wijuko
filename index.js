c9b86e06ff290896c4416460c0fb7b7fa0af26d9
David Corne <davidcorne@gmail.com>
2025-05-05 21:26:28 +0000

    Centre the svg on the page

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ const PORT = 3000
 APP.get('/', (req, res) => {
   const puzzle = wijuko.generate(Math.random)
   const svg = wijuko.generateSVG(puzzle)
-  res.set('Content-Type', 'image/svg+xml')
-  res.send(svg)
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Wijuko</title>
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        padding-top: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    ${svg}
+  </body>
+  </html>
+`;
+
+res.send(html);
 })
 
 APP.listen(PORT, () => {
