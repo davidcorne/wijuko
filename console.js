8d2148a58ef4b26328b9e665ffb6194bcb9c6757
David Corne <davidcorne@gmail.com>
2025-05-05 19:07:28 +0000

    Remove some commented out code

--- console.js
+++ console.js
@@ -2,16 +2,6 @@ const wijuko = require('./wijuko')
 
 const main = function () {
   const puzzleRand = wijuko.generate(Math.random)
-  // // Print the puzzle solved
-  // let printGridNumber = true
-  // puzzleRand.prettyPrint(console.log, printGridNumber)
-  // // Print 20 blank lines
-  // for (let i = 0; i < 20; i++) {
-  //   console.log('')
-  // }
-  // // Print the puzzle with the grid numbers hidden
-  // printGridNumber = false
-  // puzzleRand.prettyPrint(console.log, printGridNumber)
   const puzzleAsSVG = wijuko.generateSVG(puzzleRand)
   console.log(puzzleAsSVG)
 }
